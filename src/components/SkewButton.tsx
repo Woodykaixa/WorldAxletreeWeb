076e11c0b66e7c9135136a690a0cca6dc411ee25
kaixa <690750353@qq.com>
2021-09-22 22:33:34 +0800

    feat(*): SkewButton增加hover效果

--- src/components/SkewButton.tsx
+++ src/components/SkewButton.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 export function SkewButton({ children, skewX, className }: { children: ReactNode; skewX: number; className?: string }) {
   return (
     <button
-      className={'text-5xl font-bold bg-red-500 py-10 px-10 ' + className}
+      className={'text-5xl font-bold bg-red-500 hover:bg-red-400 py-10 px-10 ' + className}
       style={{ transform: `skewX(${skewX}deg)` }}
     >
       <div className='mx-15' style={{ transform: `skewX(${-skewX}deg)` }}>
