01bafbc9932797b5d96016980b18400d15ace1f7
kaixa <690750353@qq.com>
2022-01-26 23:42:13 +0800

    feat(editor): add custom plugins and frontmatter

--- pages/admin/editor.tsx
+++ pages/admin/editor.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { render } from 'react-dom';
-import { Editor } from '@bytemd/react';
+import { Editor, EditorProps } from '@bytemd/react';
 import gfm from '@bytemd/plugin-gfm';
 import footnotes from '@bytemd/plugin-footnotes';
+import frontmatter from '@bytemd/plugin-frontmatter';
 import styled from 'styled-components';
 import { Typography } from 'antd';
-import { useRouter } from 'next/router';
+import type { BytemdPlugin } from 'bytemd';
+import zhHans from 'bytemd/lib/locales/zh_Hans.json';
+import { UploadOutlined } from '@ant-design/icons';
+import { renderToString } from 'react-dom/server';
 
 const EditorStyleOverride = styled.div`
   & .bytemd {
@@ -16,8 +19,18 @@ const EditorStyleOverride = styled.div`
     top: 120px;
     height: calc(100vh - 120px) !important;
   }
+
+  & .bytemd-toolbar-right [bytemd-tippy-path='5'] {
+    display: none;
+  }
+
+  /* WTF? bytemd set vertical-align: top and help panel got overflow??? */
+  & .bytemd-help ul div {
+    vertical-align: baseline;
+  }
 `;
-const plugins = [gfm(), footnotes()];
+
+const plugins = [gfm(), footnotes(), frontmatter(), upload(), parseFrontmatter()];
 
 export default function ArticleEditor() {
   const [value, setValue] = useState('');
@@ -26,6 +39,7 @@ export default function ArticleEditor() {
       <EditorStyleOverride>
         <Typography>
           <Editor
+            locale={zhHans}
             editorConfig={{
               lineNumbers: true,
               cursorScrollMargin: 100,
@@ -40,4 +54,32 @@ export default function ArticleEditor() {
       </EditorStyleOverride>
     </div>
   );
+}
+
+function upload(): BytemdPlugin {
+  return {
+    actions: [
+      {
+        title: '上传文章',
+        icon: renderToString(<UploadOutlined />),
+        handler: {
+          type: 'action',
+          click: ctx => {
+            const text = ctx.editor.getValue();
+
+            console.log('text', text);
+          },
+        },
+      },
+    ],
+  };
+}
+
+function parseFrontmatter(): BytemdPlugin {
+  return {
+    rehype: p =>
+      p.use(() => (tree, vfile) => {
+        console.log('vfile', vfile);
+      }),
+  };
 }
