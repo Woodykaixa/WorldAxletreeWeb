e808b043cdb952981fa909a77e27731e94cd4ee7
kaixa <690750353@qq.com>
2022-01-26 22:04:07 +0800

    feat(editor): line number

--- pages/admin/editor.tsx
+++ pages/admin/editor.tsx
@@ -5,6 +5,8 @@ import gfm from '@bytemd/plugin-gfm';
 import footnotes from '@bytemd/plugin-footnotes';
 import styled from 'styled-components';
 import { Typography } from 'antd';
+import { useRouter } from 'next/router';
+
 const EditorStyleOverride = styled.div`
   & .bytemd {
     height: calc(100vh - 120px);
@@ -20,10 +22,14 @@ const plugins = [gfm(), footnotes()];
 export default function ArticleEditor() {
   const [value, setValue] = useState('');
   return (
-    <div className='w-full mt-4'>
+    <div className='w-full'>
       <EditorStyleOverride>
         <Typography>
           <Editor
+            editorConfig={{
+              lineNumbers: true,
+              cursorScrollMargin: 100,
+            }}
             value={value}
             plugins={plugins}
             onChange={v => {
