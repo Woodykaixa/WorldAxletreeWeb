3a17e820de5d5f9b8a8ad16dbd22d88127382620
kaixa <690750353@qq.com>
2022-01-26 11:37:40 +0800

    feat(Header): responsive header

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ export function Header() {
       </div>
       <div className='w-4  '></div>
       <HeaderMenu layout={HeaderLayout} />
-      <div className='flex-1 flex justify-center'>
+      <div className='m-4 1920:m-0 1920:flex-1 flex justify-center'>
         <button className='border-white border-2 px-6 py-2 hover:bg-gray-700'>简体中文</button>
       </div>
       <Image
@@ -89,7 +89,7 @@ const HeaderLayout: HeaderMenuProps['layout'] = [
 
 function HeaderMenu({ layout }: HeaderMenuProps) {
   return (
-    <ul className='col-span-2 flex w-2/5 2xl:w-1/3 justify-between text-white'>
+    <ul className='flex-1 flex w-2/5 2xl:w-1/3 justify-between text-white'>
       {layout.map(menu => (
         <li key={menu.key}>
           <Dropdown
