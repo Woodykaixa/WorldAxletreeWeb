2669c9f2daea61d452bacc423570d88efcea2dad
kaixa <690750353@qq.com>
2022-01-27 20:18:30 +0800

    feat(news): use infinite scroll

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,8 +1,9 @@
 import { Container } from '@/components';
 import { SEOHeaders } from '@/components/SEO';
-import { List, Avatar, Space } from 'antd';
+import { List, Avatar, Space, Skeleton } from 'antd';
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
-import * as React from 'react';
+import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 type Item = {
   href: string;
@@ -22,48 +23,85 @@ for (let i = 0; i < 23; i++) {
   });
 }
 export default function NewsIndex() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([] as Item[]);
+
+  const loadData = async () => {
+    if (loading) {
+      return;
+    }
+    try {
+      const response = await fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo');
+      const json = await response.json();
+      if (json.error) {
+        throw json.error;
+      }
+
+      setData([
+        ...data,
+        ...json.results.map((o: any) => ({
+          href: 'https://ant.design',
+          title: o.name.last,
+          description: o.email,
+          content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        })),
+      ]);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
     <>
       <SEOHeaders.Index title='资讯 - 世界轴承' keywords={[]} />
       <Container background='/assets/gw01.webp' preloadBackground>
-        <List
-          itemLayout='vertical'
-          size='large'
-          locale={{
-            emptyText: '暂无数据',
-          }}
-          pagination={{
-            onChange: page => {
-              console.log(page);
-            },
-            pageSize: 4,
-          }}
-          dataSource={listData}
-          renderItem={item => (
-            <div className='bg-[#686868] m-4 mb-8'>
-              <List.Item
-                key={item.title}
-                actions={[<Space key='date'>11年45月14日</Space>]}
-                extra={
-                  <img
-                    width={272}
-                    alt='logo'
-                    src='https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
-                  />
-                }
-              >
-                <List.Item.Meta
-                  title={
-                    <a href={item.href} className='text-2xl'>
-                      {item.title}
-                    </a>
-                  }
-                />
-                <div className='break-all text-xl '>{item.content}</div>
-              </List.Item>
-            </div>
-          )}
-        />
+        <div id='news-list'>
+          <InfiniteScroll
+            hasMore={data.length < 50}
+            next={loadData}
+            dataLength={data.length}
+            loader={<Skeleton paragraph={{ rows: 2 }} active></Skeleton>}
+          >
+            <List
+              itemLayout='vertical'
+              size='large'
+              locale={{
+                emptyText: '暂无数据',
+              }}
+              dataSource={data}
+              renderItem={item => (
+                <div className='bg-[#686868] m-4 mb-8'>
+                  <List.Item
+                    key={item.title}
+                    actions={[<Space key='date'>11年45月14日</Space>]}
+                    extra={
+                      <img
+                        width={272}
+                        alt='logo'
+                        src='https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
+                      />
+                    }
+                  >
+                    <List.Item.Meta
+                      title={
+                        <a href={item.href} className='text-2xl'>
+                          {item.title}
+                        </a>
+                      }
+                    />
+                    <div className='break-all text-xl '>{item.content}</div>
+                  </List.Item>
+                </div>
+              )}
+            />
+          </InfiniteScroll>
+        </div>
       </Container>
     </>
   );
