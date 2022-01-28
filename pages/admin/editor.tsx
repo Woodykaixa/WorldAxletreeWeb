48e1b47c7cab9d8fcd2d5587e5c6179456fad055
kaixa <690750353@qq.com>
2022-01-29 00:05:31 +0800

    fix(editor): set plugin after render

--- pages/admin/editor.tsx
+++ pages/admin/editor.tsx
@@ -1,66 +1,69 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Editor } from '@bytemd/react';
 import gfm from '@bytemd/plugin-gfm';
 import footnotes from '@bytemd/plugin-footnotes';
 import frontmatter from '@bytemd/plugin-frontmatter';
-import { message, notification, Typography } from 'antd';
+import { notification, Typography } from 'antd';
 import zhHans from 'bytemd/lib/locales/zh_Hans.json';
 import { EditorStyle, upload, consumeMeta } from '@/components/editor';
 import { validateAndUpload } from '@/lib/meta';
+import { BytemdPlugin } from 'bytemd';
 
 let meta: {
   value: object | null | string;
 } = {
   value: null,
 };
-
+const onUpload = (text: string) => {
+  if (!meta.value) {
+    notification.error({
+      message: '未填写元数据',
+      description: '我们使用 frontmatter 语法编写的元数据来识别上传的文章内容',
+    });
+    return;
+  }
+  if (typeof meta.value === 'string') {
+    notification.error({
+      message: '元数据应该为对象而不是字符串',
+      description: '请参考 yaml 语法，编写对象类型的元数据',
+    });
+    return;
+  }
+  validateAndUpload(meta.value as any, text)
+    .then(res => {
+      notification.success({
+        message: '上传成功',
+      });
+      console.log('meta', meta.value);
+      console.log('api return', res);
+    })
+    .catch(err => {
+      const error = err as Error;
+      notification.error({
+        message: error.name,
+        description: error.message,
+      });
+      console.error(error);
+    });
+};
 export default function ArticleEditor() {
   const [value, setValue] = useState('');
-  const onUpload = useCallback((text: string) => {
-    if (!meta.value) {
-      notification.error({
-        message: '未填写元数据',
-        description: '我们使用 frontmatter 语法编写的元数据来识别上传的文章内容',
-      });
-      return;
-    }
-    if (typeof meta.value === 'string') {
-      notification.error({
-        message: '元数据应该为对象而不是字符串',
-        description: '请参考 yaml 语法，编写对象类型的元数据',
-      });
-      return;
-    }
-    validateAndUpload(meta.value as any, text)
-      .then(res => {
-        notification.success({
-          message: '上传成功',
-        });
-        console.log('meta', meta.value);
-        console.log('api return', res);
-      })
-      .catch(err => {
-        const error = err as Error;
-        notification.error({
-          message: error.name,
-          description: error.message,
-        });
-        console.error(error);
-      });
+  const [editorPlugins, setPlugin] = useState<BytemdPlugin[]>([]);
+  useEffect(() => {
+    setPlugin([
+      gfm(),
+      footnotes(),
+      frontmatter(),
+      upload({
+        onUpload,
+      }),
+      consumeMeta({
+        onReceiveMeta(m) {
+          meta.value = m;
+        },
+      }),
+    ]);
   }, []);
-  const EditorPlugins = [
-    gfm(),
-    footnotes(),
-    frontmatter(),
-    upload({
-      onUpload,
-    }),
-    consumeMeta({
-      onReceiveMeta(m) {
-        meta.value = m;
-      },
-    }),
-  ];
   return (
     <div className='w-full'>
       <EditorStyle>
@@ -72,7 +75,7 @@ export default function ArticleEditor() {
               cursorScrollMargin: 100,
             }}
             value={value}
-            plugins={EditorPlugins}
+            plugins={editorPlugins}
             onChange={v => {
               setValue(v);
             }}
