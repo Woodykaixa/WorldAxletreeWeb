b400006e4582937d04c1cc4f3b0e65239f835b81
kaixa <690750353@qq.com>
2022-01-28 18:16:19 +0800

    feat(admin): style

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -6,16 +6,17 @@ export default function AdminIndex() {
   return (
     <Container>
       <div className='flex'>
-        <Menu theme='dark' style={{ width: 256 }} mode='inline'>
+        <Menu theme='dark' style={{ width: 256 }} mode='inline' className='bg-[#5f5f5f] mr-4' selectedKeys={['']}>
           <Menu.Item key='editor'>
             <Link href='/admin/editor'>编辑器</Link>
           </Menu.Item>
-
+          <Menu.Item key='placeholder1'></Menu.Item>
+          <Menu.Item key='placeholder2'></Menu.Item>
           <Menu.Item key='unauth'>
             <Link href='/api/unauth'>退出管理员登录</Link>
           </Menu.Item>
         </Menu>
-        你好，管理员
+        <div>你好，管理员</div>
       </div>
     </Container>
   );
