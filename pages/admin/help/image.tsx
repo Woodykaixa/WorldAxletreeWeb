4453fc5ce5d65871023df240f7482a8fc2d76994
kaixa <690750353@qq.com>
2022-01-30 01:13:27 +0800

    feat(help): write editor help

---
+++ pages/admin/help/image.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@/components';
+import Head from 'next/head';
+export default function ImageWallHelp() {
+  return (
+    <>
+      <Head>
+        <title>照片库使用说明</title>
+      </Head>
+      <Container>还没写</Container>
+    </>
+  );
+}
