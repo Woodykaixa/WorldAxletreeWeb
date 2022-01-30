f7221424253a3ed6ff43ce11998cd85d51c14a3c
kaixa <690750353@qq.com>
2022-01-30 19:28:02 +0800

    fix(game-ver): fix logical error in greater cmp

--- lib/game-ver.ts
+++ lib/game-ver.ts
@@ -9,10 +9,20 @@ export type Version = {
  * @return true, if a > b
  */
 export function greater(a: Version, b: Version): boolean {
-  if (a.majorVersion <= b.majorVersion) {
+  if (a.majorVersion > b.majorVersion) {
+    console.log('a.majorVersion > b.majorVersion');
+    return true;
+  }
+  if (a.majorVersion < b.majorVersion) {
+    console.log('a.majorVersion < b.majorVersion');
     return false;
   }
-  if (a.minorVersion <= b.minorVersion) {
+  if (a.minorVersion > b.minorVersion) {
+    console.log('a.minorVersion > b.minorVersion');
+    return true;
+  }
+  if (a.minorVersion < b.minorVersion) {
+    console.log('a.minorVersion < b.minorVersion');
     return false;
   }
   return a.patchVersion > b.patchVersion;
