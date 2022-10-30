530b9c87c981c84c88d482388e66790b9873fd66
kaixa <690750353@qq.com>
2022-10-30 19:58:27 +0800

    chore: remove less support

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const withLess = require('next-with-less');
-
 /** @type {import('next').NextConfig} */
 module.exports = {
-  ...withLess({
-    lessLoaderOptions: {
-      /* ... */
-    },
-  }),
   reactStrictMode: true,
   images: {
     domains: ['wa-staging.xkein.cn', 'worldaxletree.xkein.cn', 'localhost'],
